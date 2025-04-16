3e70fbe2f06944530754f3679781c1386b751c6a
Alexei <avenge.now.r@mail.ru>
2025-04-16 22:41:06 +0300

    feat(*): start second task

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import Layout from '~/components/Layout';
 import { useGetPostsQuery } from '~/query/services/posts.ts';
 
 function App() {
+    // 2 sprint
     const { data: _data, isLoading: _isLoading } = useGetPostsQuery();
 
     return (
